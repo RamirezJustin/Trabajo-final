d3168c4845edbae3c3686adea861c2933346b85f
Justin <ramirezjustin0210@gmail.com>
2021-11-17 18:46:58 -0500

    se agrego el evento mousemove en JavaScript

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -3,6 +3,10 @@ addEventListener('load', init);
 function click(){
     console.log('Diste click');
 }
+function mousemove(){
+    console.log('Hiciste Mousemove');
+}
 function init() {
     document.getElementById ('nombre').addEventListener('click', click);
+    document.getElementsByClassName ('contenido'),addEventListener('mousemove', mousemove)
 }
